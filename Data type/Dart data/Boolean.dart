31d00d5fded57d44b0261be9c05da8c35ea12ce0
aachalvishwakarma00 <124452413+aachalvishwakarma00@users.noreply.github.com>
2023-07-20 19:34:10 +0530

    Delete Data type directory

--- Data type/Dart data/Boolean.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-String str = 'Coding is ';
-String str1 = 'Fun';
-	
-bool val = (str==str1);
-print (val);
-}
--- Data type/Dart data/String.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-	
-	String string = 'Welcome''to''University';
-	String str = 'Coding is ';
-	String str1 = 'Fun';
-	print (string);
-	print (str + str1);
-}
